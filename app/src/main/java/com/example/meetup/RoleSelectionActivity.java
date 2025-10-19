0976e5562b515da42bf781f80d479aebb397c5e0
mfrantsuzova <frantsuzovama@student.bmstu.ru>
2025-10-19 13:57:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/meetup/RoleSelectionActivity.java
@@ -0,0 +1,15 @@
+package com.example.meetup;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RoleSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role_selection);
+    }
+}
